dbecbcae68d0bc9e09c1d966ee979508e38f9b87
Michael Dimchuk <michaeldimchuk@gmail.com>
2017-09-17 19:25:42 +0800

    Enabled request caching

--- news-front/src/main/java/io/md/news/config/ApplicationConfig.java
+++ news-front/src/main/java/io/md/news/config/ApplicationConfig.java
@@ -5,11 +5,16 @@ import io.md.news.controller.PagedNewsController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.concurrent.TimeUnit;
 
 
 @Import(MockApplicationConfig.class)
 @Configuration
-public class ApplicationConfig {
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
     @Bean
     public NewsController newsController() {
         return new NewsController();
@@ -18,5 +23,15 @@ public class ApplicationConfig {
     @Bean
     public PagedNewsController pagedNewsController() {
         return new PagedNewsController();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.DAYS));
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.DAYS));
     }
 }
